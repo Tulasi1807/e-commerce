e7debf32a018b1709831fe1c47b978e13b20ae7a
Tulasi1807 <66505367+Tulasi1807@users.noreply.github.com>
2021-02-01 15:57:21 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://tulasi1807.github.io/e-commerce/data/data.json").then(response=>{
     return response.json()
 }).then(data=>{
      displayData(data)
